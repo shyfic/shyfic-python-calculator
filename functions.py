94d841846ce4a7fa4a9500f275ad2c5815bde9c7
shyfic <shooterlive123@gmail.com>
2026-07-20 06:43:12 +0700

    Enhance mathematical functions with undefined checks
    Added checks for undefined results in division, exponent, and root functions.

--- functions.py
+++ functions.py
@@ -13,13 +13,26 @@ def multiplication(x, y):
     return result
 
 def division(x, y):
-    result = x / y
+    if x == 0 or y == 0:
+        result = "Undefined"
+    else:
+        result = x / y
     return result
 
 def exponent(x, y):
-    result = math.pow(x, y)
+    if x == 0 and y == -1:
+        result = "Undefined"
+    elif x == -1 and y == 0.5:
+        result = "Undefined"
+    elif x == 0 and y == 0:
+        result = "Undefined"
+    else:
+        result = math.pow(x, y)
     return result
 
 def root(x, y):
-    result = math.pow(x, 1/y)
+    if x < 0:
+        result = "Undefined"
+    else:
+        result = math.pow(x, 1/y)
     return result
