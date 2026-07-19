6ecc897a21ecc0b585f71dbaa50c2c2f8f796c67
shyfic <shooterlive123@gmail.com>
2026-07-19 23:56:27 +0700

    Add initial project files
    Added:
    - main.py with core functionality
    - functions.py with helper functions
    - .gitignore to exclude Python cache files
    - README with setup instructions
    - LICENSE to allow others to use, modify, and distribute

---
+++ functions.py
@@ -0,0 +1,25 @@
+import math
+
+def subtraction(x, y):
+    result = x - y
+    return result
+
+def addition(x, y):
+    result = x + y
+    return result
+
+def multiplication(x, y):
+    result = x * y
+    return result
+
+def division(x, y):
+    result = x / y
+    return result
+
+def exponent(x, y):
+    result = math.pow(x, y)
+    return result
+
+def root(x, y):
+    result = math.pow(x, 1/y)
+    return result
---
+++ main.py
@@ -0,0 +1,31 @@
+import math
+import functions
+
+x = float(input("Enter the value of X: "))
+print(f"Your X value is {x}")
+y = float(input("Enter the value of Y: "))
+print(f"Your Y value is {y}")
+choice = int(input("Enter the operation:\n(1) for addition\n(2) for subtraction\n"
+"(3) for multiplication\n(4) for division\n(5) for exponent\n(6) for root\n"))
+
+def operation(choice, x, y):
+    match choice:
+        case 1:
+            return functions.addition(x, y)
+        case 2:
+            return functions.subtraction(x, y)
+        case 3:
+            return functions.multiplication(x, y)
+        case 4:
+            return functions.division(x, y)
+        case 5:
+            return functions.exponent(x, y)
+        case 6:
+            return functions.root(x, y)
+        case _:
+            print("Please input a correct option (1-6)")
+            return None 
+
+result = operation(choice, x, y)
+if result is not None:
+    print(f"The result is: {result}")
