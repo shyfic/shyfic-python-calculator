e96d61600813bd2afb33f5fa562617edd07b24d7
shyfic <shooterlive123@gmail.com>
2026-07-22 22:19:09 +0700

    Refactor button creation in calculator GUI

--- gui.py
+++ gui.py
@@ -1,10 +1,18 @@
 import tkinter as tk
 from tkinter import ttk
 
+
 root = tk.Tk()
 
 root.title("Calculator (built in python)")
-root.geometry("400x400")
+root.geometry("400x400")    
+
+button_names = [
+    "9", "8", "7", "CE", "xʸ",
+    "6", "5", "4", "-", "ʸ√x",
+    "3", "2", "1", "+", "x",
+    "0", ".", "C", "Exe", "/"
+]
 
 operation = tk.Label(
     root,
@@ -15,12 +23,15 @@ operation = tk.Label(
 
 operation.grid(row=0, column=0, columnspan=5, sticky="nsew", padx=5, pady=5)
 
+index = 0
+
 for row in range(1, 5):
     for col in range(5):
         operation_btn = tk.Button(
-            root, text="x"
+            root, text=button_names[index]
         )
         operation_btn.grid(row=row, column=col, sticky="nsew", padx=2, pady=2)
+        index += 1
 
 for col in range(5):
     root.grid_columnconfigure(col, weight=1)
